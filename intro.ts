7d9de322387438c5f1f37a3eb8681b20b5c28d00
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-11 19:26:44 +0300

    valueTypes_1

--- intro.ts
+++ intro.ts
@@ -1,5 +1,28 @@
-function greeter(name:string){
-    return "Hello " + name 
+function greeter(name: string) {
+  return "Hello " + name;
 }
 
-let message = greeter('Barış')
+let message = greeter("Barış");
+
+console.log(message);
+
+let number: number;
+number = 10;
+number = 10.4;
+
+let city : string = "Adana";
+city = "Ankara";
+city = "Kocaeli";
+//city = 10  // false
+
+let isTrue : boolean = true
+isTrue = true
+isTrue = false
+
+let numbers: number[] = [1,2,3,4]
+let numbers2 : Array<number> = [1,2,3,4]
+
+let array : [number,string] = [2,"İstanbul"]
+
+enum Colors {Kirmizi=1,Siyah,Mavi}
+let color : Colors = Colors.Kirmizi
