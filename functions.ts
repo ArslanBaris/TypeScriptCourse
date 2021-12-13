e1bdce03eebb725ee1e80eb0e3384ea8011d2e94
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-13 15:06:56 +0300

    feat: functions.ts added.

---
+++ functions.ts
@@ -0,0 +1,21 @@
+function topla(x,y){
+    return x+y;
+}
+
+function topla2(x:number, y:number):number {
+    return x+y;
+}
+
+let topla3 = function(x:number,y:number):number{
+    return x+y;
+}
+
+console.log(topla(2,3));
+
+console.log(topla("Ankara",2));
+
+console.log(topla2(2,3));
+
+console.log(topla3(2,3));
+
+
