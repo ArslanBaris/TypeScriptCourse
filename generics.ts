c1d5c9f87b519f635641fe710a2cc9ad6f4771f4
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-13 21:21:59 +0300

    generics

---
+++ generics.ts
@@ -0,0 +1,23 @@
+function deger(x: number): number {
+  return x;
+}
+
+function deger2(x: string): string {
+  return x;
+}
+
+let sayi = deger(10);
+console.log(sayi);
+
+let sehir = deger2("Ankara");
+console.log(sehir);
+
+function deger3<T>(x: T): T {
+    return x;
+}
+
+let sayi2 = deger3<number>(10);
+console.log(sayi2);
+
+let sehir2 = deger3<string>("Ankara");
+console.log(sehir2);
