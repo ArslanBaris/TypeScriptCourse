9e64035412d37c7beb4e6815e956d457829d5991
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-12-13 15:10:25 +0300

    optional and default parameters

--- functions.ts
+++ functions.ts
@@ -18,4 +18,16 @@ console.log(topla2(2,3));
 
 console.log(topla3(2,3));
 
+function topla4(x:number, y:number=4):number {  // default parameters
+    return x+y;
+}
 
+console.log(topla4(3));
+
+function topla5(x:number, y?:number):number {  // optional parameters
+    if(y)
+        return x+y;
+    return x;
+}
+console.log(topla5(3));     // 3
+console.log(topla5(3,5));   // 8
